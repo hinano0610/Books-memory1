10c888c93f575d139bdbad00cafe39c8f2529917
hinano2295 <136045154+hinano0610@users.noreply.github.com>
2024-05-22 12:35:03 +0000

    new search

--- src/components/SearchBooks.jsx
+++ src/components/SearchBooks.jsx
@@ -1,24 +1,29 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import "./../styles.css";
 
 export const SearchBooks = () => {
+  //本の情報を取得
   const [searchQuery, setSearchQuery] = useState("");
+
   const [searchResults, setSearchResults] = useState([]);
 
+  //searchQueryが変更された時にconsoleで変更を確認できる
   useEffect(() => {
     console.log("searchQuery updated:", searchQuery);
   }, [searchQuery]);
 
+  //変更をsearchQueryに設定する
   const handleInputChange = (event) => {
-    // console.log("event");
     setSearchQuery(event.target.value);
   };
 
+  //検索内容をAPIからとってくる
   const search = async () => {
     try {
       const response = await fetch(
-        `https://www.googleapis.com/books/v1/volumes?q=${searchQuery}`
+        `https://www.googleapis.com/books/v1/volumes?q=${searchQuery}`,
       );
       const data = await response.json();
       console.log(data);
@@ -30,17 +35,22 @@ export const SearchBooks = () => {
 
   return (
     <div className="Search">
-      <h1>BookMemory</h1>
-      <input
-        placeholder="登録する本を入力"
-        value={searchQuery}
-        onChange={handleInputChange}
-      />
-      <button onClick={search}>検索</button>
+      <h1>本の検索</h1>
+      <div className="bookName">
+        <input
+          className="searchtitle"
+          placeholder="登録する本を入力"
+          value={searchQuery}
+          onChange={handleInputChange}
+        />
+        <button onClick={search} className="searchbutton">
+          検索
+        </button>
+      </div>
       <ul>
         {searchResults.map((book) => (
           <li key={book.id}>
-            <Link to={`/book/${book.id}`}>
+            <Link to={`/book/${book.id}`} state={{ bookData: book }}>
               <strong>{book.volumeInfo.title}</strong>
               {book.volumeInfo.authors && book.volumeInfo.authors.join(",")}
               <br />
@@ -54,6 +64,9 @@ export const SearchBooks = () => {
           </li>
         ))}
       </ul>
+      <Link to={"/"}>
+        <button>ホームに戻る</button>
+      </Link>
     </div>
   );
 };
